0d106b22b0eb579bd6e6338a854b314f2566c826
tushar-19459 <19459tusharraj@gmail.com>
2025-08-27 16:12:24 +0530

    Stack and Queue

--- Queue/simpleQueue.py
+++ Queue/simpleQueue.py
@@ -1,4 +1,32 @@
-class fixQueue:
+class SimpleQueue:
+    def __init__(self):
+        self.data = []
+    
+    def isEmpty(self):
+        return len(self.data) == 0
+
+    def enqueue(self,ele):
+        self.data.append(ele)
+        return self.data
+    
+    def dequeue(self):
+        if not self.isEmpty():
+            ele = self.data[0]
+            self.data = self.data[1:]
+            return self.data
+    
+    def findMax(self):
+        if not self.isEmpty():
+            max = self.data[0]
+            for i in range(1,len(self.data)):
+                 if max < self.data[i]:
+                    max = self.data[i]
+            return max
+    
+    def get(self):
+        return self.data
+        
+class FixedQueue:
     def __init__(self,size):
         self.size= size
         self.data = [None]*size
@@ -14,7 +42,7 @@ class fixQueue:
             self.data[self.rear] = ele
             self.rear = (self.rear+1)%self.size
             self.count +=1
-            print(self.data)
+            return self.data
         else:
             print(" full ")
     
@@ -23,12 +51,19 @@ class fixQueue:
             self.data[self.front] = None
             self.count -= 1
             self.front = (self.front+1)%self.size
-            print(self.data)
+            return self.data
         else:
             print(" empty ")
-
     
-class infQueue:
+    def findMax(self):
+        if not self.isEmpty():
+            max = self.data[0]
+            for i in range(1,len(self.data)):
+                 if max < self.data[i]:
+                    max = self.data[i]
+            return max
+
+class FlexyQueue:
     def __init__(self):
         self.size = 2
         self.data = [None]*self.size
@@ -57,17 +92,152 @@ class infQueue:
         self.rear = (self.rear+1)%len(self.data)
         self.count+=1
 
-        print(self.data)
+        return self.data
     
     def dequeue(self):
         if  self.isEmpty():
-            print(" empty ")
+            pass
+            # print(" empty ")
         else:
             if self.count <= len(self.data)//2 :
                 self.rear = len(self.data)//2
-                print('rear ',self.rear)
                 self.resize(len(self.data)//2)
             self.data[self.front] = None
             self.front = (self.front+1)%len(self.data)
             self.count -= 1
-        print(self.data)
+        return self.data
+
+class SatckUsingQueue:
+    def __init__(self,size):
+        self.size= size
+        self.data = [None]*size
+        self.count = 0
+        self.rear = 0
+        self.front = 0
+
+        self.tempdata = [None]*size
+        self.temprear = 0
+
+    def isEmpty(self):
+        return self.count == 0
+
+    def enqueue(self,ele):
+        if self.count != len(self.data):
+            self.data[self.rear] = ele
+            self.rear = (self.rear+1)%self.size
+            self.count +=1
+            return self.data
+        else:
+            return "overflow"
+    
+    def dequeue(self):
+        if not self.isEmpty():
+            ele = self.data[self.front]  
+            self.data[self.front] = None
+            self.count -= 1
+            self.front = (self.front+1)%self.size
+            return ele
+        else:
+            print(" empty ")
+
+    def pop(self):
+        if not self.isEmpty():
+            front = self.front
+            n = self.count
+            pop = None
+            for i in range(n-1):
+                self.tempdata[i] = self.dequeue()
+            pop = self.dequeue()
+            self.rear = self.count % self.size
+            self.data = self.tempdata
+            self.tempdata = [None]*self.size
+            self.count = n - 1
+            self.front = front
+            return pop
+        else:
+            return "Empty"
+        
+class Rotate:
+    def __init__(self,size):
+        self.size= size
+        self.data = [None]*size
+        self.count = 0
+        self.rear = 0
+        self.front = 0
+    
+    def isEmpty(self):
+        return self.count == 0
+
+    def enqueue(self,ele):
+        if self.count != len(self.data):
+            self.data[self.rear] = ele
+            self.rear = (self.rear+1)%self.size
+            self.count +=1
+            return self.data
+        else:
+            return "full"
+    
+    def dequeue(self):
+        if not self.isEmpty():
+            self.data[self.front] = None
+            self.count -= 1
+            self.front = (self.front+1)%self.size
+            return self.data
+        else:
+            return "empty"
+    
+    def findMax(self):
+        if not self.isEmpty():
+            max = self.data[0]
+            for i in range(1,len(self.data)):
+                 if max < self.data[i]:
+                    max = self.data[i]
+            return max
+        
+    def rotateEle(self):
+        temp = self.data[:]
+        self.data +=temp[::-1]
+        self.size = self.size*2
+        self.count = self.count*2
+        self.rear = self.rear*2%self.size
+        return self.data
+
+class QueueUsingStack:
+    def __init__(self,size):
+        self.data = [None]*size
+        self.top = 0
+        self.count = 0
+    
+    def push(self,ele):
+        if not self.isfull():
+            self.data[self.top] = ele
+            self.top += 1
+            self.count +=1
+            return self.data
+        
+    def pop(self):
+        if not self.isEmpty():
+            self.top -= 1
+            self.count -=1
+            ele = self.data[self.top]
+            self.data[self.top] = None
+            return ele
+    
+    def dequeue(self):
+        if not self.isEmpty():
+            temp = []
+            n = self.count 
+            while n!=1:
+                temp.append(self.pop())
+                n -=1
+            self.pop()
+            while len(temp) !=0:
+                self.push(temp.pop())
+        return self.data
+        
+
+    def isfull(self):
+        return self.top == len(self.data)
+    
+    def isEmpty(self):
+        return self.top == 0
--- Queue/testQueue.py
+++ Queue/testQueue.py
@@ -1,46 +1,120 @@
-from simpleQueue import fixQueue,infQueue
-def testFixQueue():
-    queue = fixQueue(5)
-    queue.enqueue(1)
-    queue.enqueue(2)
-    queue.enqueue(3)
-    queue.enqueue(4)
-    queue.enqueue(5)
-    queue.dequeue()
-    queue.enqueue(6)
-    queue.dequeue()
-    queue.dequeue()
-    queue.enqueue(5)
-    queue.enqueue(6)
-    queue.enqueue(8)
-    queue.dequeue()
-# testFixQueue()
+from simpleQueue import FixedQueue,FlexyQueue ,SimpleQueue ,SatckUsingQueue,Rotate,QueueUsingStack
+#1
+def testsimpleQueue():
+    queue = SimpleQueue()
+    assert queue.isEmpty() == True
+    assert queue.enqueue(10) == [10]
+    assert queue.enqueue(20) == [10,20]
+    assert queue.enqueue(30) == [10,20,30]
+    assert queue.dequeue() == [20,30]
+    assert queue.dequeue() == [30]
+    assert queue.dequeue() == []
+# testsimpleQueue()
 
-def testInf():
-    queue = infQueue()
-    queue.enqueue(1) 
-    queue.enqueue(2) 
-    queue.enqueue(3) 
-    queue.enqueue(4) 
-    queue.enqueue(5) 
-    queue.dequeue() 
-    queue.enqueue(6) 
-    queue.enqueue(6) 
-    queue.enqueue(6) 
-    queue.enqueue(6) 
-    queue.enqueue(7) 
-    queue.enqueue(7) 
-    queue.enqueue(7) 
-    queue.dequeue() 
-    queue.dequeue() 
-    queue.dequeue() 
-    queue.dequeue() 
-    queue.dequeue() 
-    queue.dequeue() 
-    queue.dequeue() 
-    queue.dequeue() 
-    queue.dequeue() 
-    queue.dequeue() 
-    queue.dequeue() 
-    queue.dequeue() 
-testInf()
+#2
+def testFixedQueue():
+    queue = FixedQueue(5)
+    assert queue.enqueue(1)== [1,None,None,None,None]
+    assert queue.enqueue(2)== [1,2,None,None,None]
+    assert queue.enqueue(3)== [1,2,3,None,None]
+    assert queue.enqueue(4)== [1,2,3,4,None]
+    assert queue.enqueue(5)== [1,2,3,4,5]
+    assert queue.dequeue()== [None,2,3,4,5]
+    assert queue.enqueue(6)== [6,2,3,4,5]
+    assert queue.dequeue()== [6,None,3,4,5]
+    assert queue.dequeue()== [6,None,None,4,5]
+    assert queue.dequeue()== [6,None,None,None,5]
+    assert queue.dequeue()== [6,None,None,None,None]
+    assert queue.dequeue()== [None,None,None,None,None]
+# testFixedQueue()
+
+#3
+def testflexyQueue():
+    queue = FlexyQueue()
+    assert queue.enqueue(1) == [1, None] 
+    assert queue.enqueue(2) == [1, 2]
+    assert queue.enqueue(3) == [1, 2, 3, None] 
+    assert queue.enqueue(4) == [1, 2, 3, 4]
+    assert queue.enqueue(5) == [1, 2, 3, 4, 5, None, None, None] 
+    assert queue.dequeue()  == [None, 2, 3, 4, 5, None, None, None]
+    assert queue.enqueue(6) == [None, 2, 3, 4, 5, 6, None, None] 
+    assert queue.enqueue(6) == [None, 2, 3, 4, 5, 6, 6, None] 
+    assert queue.enqueue(6) == [None, 2, 3, 4, 5, 6, 6, 6]
+    assert queue.enqueue(6) == [6, 2, 3, 4, 5, 6, 6, 6]
+    assert queue.enqueue(7) == [2, 3, 4, 5, 6, 6, 6, 6, 7, None, None, None, None, None, None, None]
+    assert queue.dequeue()  == [None, 3, 4, 5, 6, 6, 6, 6, 7, None, None, None, None, None, None, None]
+    assert queue.dequeue()  == [None, 4, 5, 6, 6, 6, 6, 7]
+    assert queue.dequeue()  == [None, None, 5, 6, 6, 6, 6, 7]
+    assert queue.dequeue()  == [None, None, None, 6, 6, 6, 6, 7]
+    assert queue.dequeue()  == [None, None, None, None, 6, 6, 6, 7]
+    assert queue.dequeue()  == [None, 6, 6, 7]
+    assert queue.dequeue()  == [None, None, 6, 7]
+    assert queue.dequeue()  == [None, 7]
+    assert queue.dequeue()  == [None]
+    assert queue.dequeue()  == [None]
+    assert queue.dequeue()  == [None]
+    assert queue.dequeue()  == [None] 
+# testflexyQueue()
+
+#4
+def testSatckUsingQueue():
+    stack = SatckUsingQueue(5)
+    assert stack.enqueue(10) == [10,None,None,None,None]
+    assert stack.enqueue(20) ==[10,20,None,None,None]
+    assert stack.enqueue(30) ==[10,20,30,None,None]
+    assert stack.enqueue(40) ==[10,20,30,40,None]
+    assert stack.enqueue(50) ==[10,20,30,40,50]
+    assert stack.enqueue(60) =="overflow"
+    assert stack.pop() == 50
+    assert stack.pop() == 40
+    assert stack.pop() == 30
+    assert stack.pop() == 20
+    assert stack.pop() == 10
+    assert stack.pop() == "Empty"
+    assert stack.enqueue(10) == [10,None,None,None,None]
+    assert stack.enqueue(10) == [10,10,None,None,None]
+# testSatckUsingQueue()
+
+#6
+def testrotate():
+    queue = Rotate(5)
+    assert queue.enqueue(1) == [1,None,None,None,None]
+    assert queue.enqueue(2) == [1,2,None,None,None]
+    assert queue.enqueue(3) == [1,2,3,None,None]
+    assert queue.enqueue(4) == [1,2,3,4,None]
+    assert queue.enqueue(5) == [1,2,3,4,5]
+    assert queue.rotateEle() == [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    assert queue.dequeue() == [None, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    assert queue.enqueue(11) == [11, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+    assert queue.enqueue(11) == "full"
+    assert queue.dequeue() == [11, None, 3, 4, 5, 5, 4, 3, 2, 1]
+# testrotate()
+
+#7
+def testMax():
+    queue = SimpleQueue()
+    queue.enqueue(21)
+    queue.enqueue(1)
+    queue.enqueue(40)
+    queue.enqueue(34)
+    queue.enqueue(22)
+    queue.enqueue(11)
+    queue.enqueue(2)
+    assert queue.findMax() == 40
+# testMax()
+
+def testqueueUsingStack():
+    stack = QueueUsingStack(5)
+    assert stack.push(10) ==[10, None, None, None, None]
+    stack.push(20) ==[10, 20, None, None, None]
+    stack.push(30) == [10, 20, 30, None, None]
+    stack.push(40) == [10, 20, 30, 40, None]
+    stack.push(50) == [10, 20, 30, 40, 50]
+    assert stack.pop() == 50
+    assert stack.pop() == 40
+    assert stack.dequeue() == [20, 30, None, None, None]
+    assert stack.dequeue() == [30, None,None, None, None]
+    assert stack.pop() == 30
+    assert stack.dequeue() == [None, None,None, None, None]
+
+testqueueUsingStack()
