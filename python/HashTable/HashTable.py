558e291d1e7d3e0836f40d8baf91477d5257a0ee
tushar-19459 <19459tusharraj@gmail.com>
2025-11-23 13:20:27 +0530

    hashing

--- python/HashTable/HashTable.py
+++ python/HashTable/HashTable.py
@@ -1,48 +1,47 @@
 import numpy as np
-
-class Hashhash_Table:
+class HashTable:
     class node:
         def __init__(self,id,name):
             self.id = id
             self.name = name
             self.next = None
-            
+
     def __init__(self):
-        self.hash_table_size = 11
-        self.hash_table = [None]*self.hash_table_size
+        self.hashTable_size = 11
         self.p = 13
-        self.a = np.random(1,self.p-1)
-        self.b = np.random(0,self.p-1)
-        
+        self.a = np.random.randint(1,self.p-1)
+        self.b = np.random.randint(0,self.p-1)
+        self.hashTable = [None]*self.hashTable_size
+    
     def _hash_(self,id):
         code = 0
-        id = str(id)
-        for ch in id:
-            code = code<<5
-            self.code = code+ord(ch)
+        data = str(id)
+        for i in data:
+            code = i<<5
+            code = code+ord(i)
         return code
 
-    def _compress_(self,code):
-        return (((code*self.a+self.b)%self.p)%self.hash_table_size)
-
-    def hashFunction(self,id):
+    def _compresstion_(self,code):
+        return (((code*self.a+self.b)%self.p)%self.hashTable_size)
+    
+    def hash_function(self,id):
         code = self._hash_(id)
-        bucket = self._compress_(code)
-        return bucket 
+        bucket = self._compresstion_(code)
+        return bucket
 
-    def isMenmber(self,id):
-        bucket = self.hashFunction(id)
-        current = self.hash_table[bucket]
+    def is_member(self,id):
+        bucket = self.hash_function(id)
+        current = self.hashTable[bucket]
         while current!=None:
             if current.id == id:
                 return True
             else:
                 current = current.next
         return False
-
+    
     def insert(self,id,name):
-        if not self.isMenmber(id):
-            bucket = self.hashFunction(id)
+        if not self.is_member(id):
+            bucket = self.hash_function(id)
             new_node = self.node(id,name)
-            new_node.next = self.hash_table[bucket]
-            self.hash_table[bucket] = new_node
+            new_node.next = self.hashTable[bucket]
+            self.hashTable[bucket] = new_node
